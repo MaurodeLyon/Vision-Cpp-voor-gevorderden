cc90b7edeae1a394218d11eba428aef9d5d9f7a1
Arthur Brink <arthur.brink1@gmail.com>
2017-12-11 14:39:00 +0100

    Perception x1 x2 x3
    Perceptron x1 x2 x3

--- VisionCPP/Perceptron/main.cpp
+++ VisionCPP/Perceptron/main.cpp
@@ -28,9 +28,9 @@ dd 9-9-2016
 using namespace std;
 
 
-double perceptronOutput(double x1, double x2, double W1, double W2, double threshold) {
+double perceptronOutput(double x1, double x2 ,double x3, double W1, double W2, double W3, double threshold) {
 	// bereken input
-	double input = W1*x1 + W2*x2;
+	double input = W1*x1 + W2*x2 + W3*x3;
 
 	// bepaal output
 	if (input > threshold) return 1.0;
@@ -41,14 +41,27 @@ double perceptronOutput(double x1, double x2, double W1, double W2, double thres
 void main() {
 
 	// waarheidstabel AND functie
-	double tabel[4][3] = { { 0.0, 0.0, 0.0 },
+	/*double tabel[4][3] = { { 0.0, 0.0, 0.0 },
 	{ 0.0, 1.0, 0.0 },
 	{ 1.0, 0.0, 0.0 },
-	{ 1.0, 1.0, 1.0 } };
+	{ 1.0, 1.0, 1.0 } };*/
+
+
+	double tabel[8][4] = {{0.0,0.0,0.0, 0.0},
+	{ 0.0,0.0,1.0, 1.0 },
+	{ 1.0,0.0,0.0, 0.0 },
+	{ 1.0,0.0,1.0, 1.0 },
+	{ 0.0,1.0,0.0, 0.0 },
+	{ 0.0,1.0,1.0, 1.0 },
+	{ 1.0,1.0,0.0, 1.0},
+	{ 1.0,1.0,1.0, 1.0 }
+
+	};
 
 	// startwaarden van de weegfactoren (willekeurig)
 	double W1 = 1003;
 	double W2 = 456;
+	double W3 = 768;
 
 	// startwaarde van de threshold (willekeurig)
 	double threshold = 185;
@@ -57,7 +70,8 @@ void main() {
 	double input, output, deltaOutput;
 
 	// aanpassingen
-	double deltaThreshold = 0.0, deltaW1 = 0.0, deltaW2 = 0.0;
+	double deltaThreshold = 0.0, deltaW1 = 0.0, deltaW2 = 0.0, deltaW3 = 0.0;
+	
 
 	// boolean flag wordt gehezen als output meer dan EPSILON afwijkt van verwachte output
 	bool flag = false;
@@ -65,9 +79,10 @@ void main() {
 
 	// waarheidstabel afdrukken 
 	cout << "Waarheidstabel waarvoor het perceptron wordt getraind" << endl;
-	for (int rij = 0; rij < 4; rij++) {
+	for (int rij = 0; rij < 8; rij++) {
 		cout << setw(3) << tabel[rij][0] << setw(3) << tabel[rij][1] << setw(3)
-			<< tabel[rij][2] << endl;
+			<< tabel[rij][2] << setw(3)
+			<< tabel[rij][3] << endl;
 		cout << endl;
 	}
 	cout << "Press ENTER to continue" << endl;
@@ -80,10 +95,10 @@ void main() {
 		for (int rij = 0; rij < 4; rij++) {
 
 			// bereken actuele output van het nog ongetrainde perceptron
-			output = perceptronOutput(tabel[rij][0], tabel[rij][1], W1, W2, threshold);
+			output = perceptronOutput(tabel[rij][0], tabel[rij][1], tabel[rij][2], W1, W2,W3, threshold);
 
 			// bereken de afwijking van de actuele output t.o.v. verwachte output
-			deltaOutput = (output - tabel[rij][2]);
+			deltaOutput = (output - tabel[rij][3]);
 
 			if (abs(deltaOutput) > EPSILON) {
 
@@ -92,6 +107,7 @@ void main() {
 				// berekening aanpassing W1 en W2
 				deltaW1 = -deltaOutput*tabel[rij][0];
 				deltaW2 = -deltaOutput*tabel[rij][1];
+				deltaW3 = -deltaOutput*tabel[rij][2];
 
 				// threshold, W1 en W2 aanpassen
 				threshold += deltaOutput;
@@ -109,14 +125,15 @@ void main() {
 	cout << "Berekende threshold = " << threshold << endl;
 	cout << "Berekende W1 = " << W1 << endl;
 	cout << "Berekende W2 = " << W2 << endl;
+	cout << "Berekende W3 = " << W3 << endl;
 	cout << "Press ENTER to continue" << endl;
 	cin.ignore();
 
 	// genereer waarheidstabel met getrainde perceptron
 	cout << "waarheidstabel gegenereerd met perceptron => " << endl;
-	for (int rij = 0; rij < 4; rij++) {
-		cout << setw(3) << tabel[rij][0] << setw(3) << tabel[rij][1] << setw(3)
-			<< perceptronOutput(tabel[rij][0], tabel[rij][1], W1, W2, threshold) << endl;
+	for (int rij = 0; rij < 8; rij++) {
+		cout << setw(3) << tabel[rij][0] << setw(3) << tabel[rij][1] << setw(3) << tabel[rij][2] << setw(5)
+			<< perceptronOutput(tabel[rij][0], tabel[rij][1], tabel[rij][2], W1, W2, W3, threshold) << endl;
 	}
 
 	cout << "Press ENTER to continue" << endl;
