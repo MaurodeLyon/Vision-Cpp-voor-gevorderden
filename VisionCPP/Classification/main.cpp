9cbbe2c6428c94b6daf8f23e3c0d979515baa3da
Arthur Brink <arthur.brink1@gmail.com>
2017-12-13 16:38:47 +0100

    trainingset + setup

--- VisionCPP/Classification/main.cpp
+++ VisionCPP/Classification/main.cpp
@@ -14,6 +14,33 @@ int main(int argc, char** argv)
 	string input;
 	getline(cin, input);
 
+	VideoCapture cap(1);
+	//waitKey(500);
+	if (!cap.isOpened())
+		cout << "Cannot open the video cam" << endl;
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+	Mat frame;
+	int i = 0;
+	string filename = "vis";
+	while (true)
+	{
+		cap.read(frame);
+		imshow("test", frame);
+		if (waitKey(30) == 27)
+		{
+			std::cout << "save object" << std::endl;
+			
+			//getline(cin,filename);
+			imwrite("./../Images/trainingset/" + filename + "_" + to_string(i) + ".jpg", frame);
+			i++;
+		}
+	}
+		
+
 	// Get the camera image
 	// Detect object
 	// Extract object 
